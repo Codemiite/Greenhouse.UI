f57c7d12aad35264f13e644d7b8978cc6ae41284
Codemiite <sewellray11+git@gmail.com>
2023-06-07 16:07:39 +0100

    create gulp task to import bulma css

---
+++ Greenhouse.UI/gulpfile.js
@@ -0,0 +1,15 @@
+﻿var gulp = require('gulp')
+
+var paths = {
+    webroot: './wwwroot/',
+    bulmaCss: './node_modules/bulma/css/bulma.css'
+};
+
+paths.bulmaDest = paths.webroot + 'css/';
+
+gulp.task('build:bulma', function (done) {     // Define a task called build.bulma
+    console.log('Copying Bulma Cascading Style Sheets...')
+    gulp.src(paths.bulmaCss)           // Looks for bulma css file
+        .pipe(gulp.dest(paths.bulmaDest)); // Put it in the wwwroot.
+    done();
+});
